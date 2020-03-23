8aac1c9971828fc5777d7b3fab59cd59b8f184d2
Jae Young Park <jaeyoung.p@ingstory.kr>
2020-03-23 11:47:54 +0900

    first commit

---
+++ src/main/java/com/lotto/Main.java
@@ -0,0 +1,13 @@
+package com.lotto;
+
+/**
+ * Created by qkrwpdud1@gmail.com on 2020/03/23
+ * Github : http://github.com/jypweback
+ * Description :
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("start ########################");
+    }
+}
